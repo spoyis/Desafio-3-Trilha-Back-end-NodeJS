66fa8b7d3f34df22137c48025027e44f2f259aea
spoyis <leandrooliveira14051999@gmail.com>
2023-04-05 22:53:52 -0400

    feat: added basic implementation of user signUp route

--- src/modules/user/UserController.ts
+++ src/modules/user/UserController.ts
@@ -1,6 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
 import { UserRepository } from './UserRepository';
+import UserValidator from './UserValidator';
 import UserInterface from './UserInterface';
+import AuthController from '../../utils/AuthController';
+import MakeResponse from '../../utils/MakeResponse';
+import ErrorController from '../../errors/ErrorController';
+import AppError from '../../errors/AppError';
 
 const repo = new UserRepository();
 namespace UserController{
@@ -9,9 +14,16 @@ namespace UserController{
     // TODO:
   }
 
-  export const signUp =  async(req: Request, res: Response, next : NextFunction): Promise<any> =>{
-    // TODO:
-  }
+  export const signUp =  ErrorController.catchAsync( async(req: Request, res: Response, next : NextFunction): Promise<any> =>{
+    // TODO: get cep data
+    const userData = await UserValidator.validate(req.body);
+    console.log(userData)
+
+    const userDoc = await repo.create(userData);
+    const token = await AuthController.signToken(userDoc._id);
+
+    MakeResponse.success(res, 201, "User successfully created" , token);
+  });
 
   export const signIn = async (req: Request, res: Response, next : NextFunction) : Promise<any> =>{
     // TODO:
