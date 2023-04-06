d745ea0525f294919d212962bd19a40f17792c13
spoyis <leandrooliveira14051999@gmail.com>
2023-04-05 21:18:15 -0400

    feat: added password hashing on mongoose user schema

--- src/modules/user/UserSchema.ts
+++ src/modules/user/UserSchema.ts
@@ -26,3 +26,14 @@ const userSchema = new Schema<UserInterface>({
     type: Boolean
   }
 });
+
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+	
+  next();
+});
+
+const UserModel = model('User', userSchema);
+export default UserModel
