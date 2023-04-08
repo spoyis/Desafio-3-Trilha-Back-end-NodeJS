9a3dee7631d5b5ae003af61d412076c2f59b606f
spoyis <leandrooliveira14051999@gmail.com>
2023-04-08 15:48:42 -0400

    feat: added reservation update endpoint

--- src/modules/reserve/ReserveController.ts
+++ src/modules/reserve/ReserveController.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction, query } from 'express';
 import { ReserveRepository } from './ReserveRepository';
 import { CarRepository } from '../car/CarRepository';
 import CarInterface from '../car/CarInterface';
@@ -9,7 +9,6 @@ import { HydratedDocument, FilterQuery, QueryOptions } from 'mongoose';
 import AppError from '../../errors/AppError';
 import MakeResponse from '../../utils/MakeResponse';
 
-
 const repo = new ReserveRepository();
 const carRepo = new CarRepository();
 
@@ -19,14 +18,18 @@ namespace ReserveController{
   const NO_FILTER = {};
   const NO_OPTIONS = {};
 
+  function calculateTotalCost (value_per_day : number, start_date: Date, end_date: Date): number {
+    const ONE_DAY_MS = 1000 * 60 * 60 * 24;
+    return value_per_day * Math.ceil((end_date.valueOf() - start_date.valueOf()) / ONE_DAY_MS);
+  }
+
   export const POST =  ErrorController.catchAsync( async(req: Request, res: Response, next : NextFunction): Promise<any> =>{
     const carQuery : FilterQuery<HydratedDocument<CarInterface>> = {_id: req.body.id_car}
     
     const car = await carRepo.findOne(carQuery, NO_OPTIONS);
     if(!car) return next(new AppError("No object found with given id.", 404));
 
-    const ONE_DAY_MS = 1000 * 60 * 60 * 24;
-    req.body.final_value = car!.value_per_day * Math.floor((new Date(req.body.end_date).valueOf() - new Date(req.body.start_date).valueOf()) / ONE_DAY_MS);
+    req.body.final_value = calculateTotalCost(car!.value_per_day, new Date(req.body.start_date), new Date(req.body.end_date));
 
     req.body.id_user =  (req as any).user.id ;
 
@@ -62,7 +65,26 @@ namespace ReserveController{
   })
 
   export const UPDATE =  ErrorController.catchAsync( async (req: Request, res: Response, next : NextFunction) : Promise<any> =>{
-    // TODO:
+    let updateParams: FilterQuery<HydratedDocument<ReserveInterface>>  = {_id : req.params.id};
+    let queryOptions: QueryOptions<HydratedDocument<ReserveInterface>> = {populate: {path: "id_car"}};
+
+    const reservation = await repo.findOne(updateParams, queryOptions);
+    if(!reservation) return next(new AppError("No object found with given id.", 404));
+
+    if(req.body.start_date || req.body.end_date){
+      req.body.start_date = req.body.start_date || reservation.start_date;
+      req.body.end_date = req.body.end_date || reservation.end_date;
+
+      req.body.final_value = calculateTotalCost((reservation as any).id_car.value_per_day, new Date(req.body.start_date), new Date(req.body.end_date));
+    }
+
+    console.log(req.body)
+    
+    await ReserveValidator.validateUPDATE(req.body);
+    await repo.update(req.params.id, req.body );
+    // TODO: DISALLOW UPDATES IF THE GIVEN CAR ALREADY HAS A RESERVATION BOOKED WHICH INTERSECTS THE NEW TIMEFRAME
+
+    MakeResponse.success(res, 200, "Reservation successfully updated");
   });
 }
 
