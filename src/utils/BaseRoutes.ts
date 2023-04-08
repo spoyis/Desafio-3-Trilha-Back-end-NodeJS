b2fb967296d50f24a0a2a6e69b74075f62d40fd4
spoyis <leandrooliveira14051999@gmail.com>
2023-04-08 01:22:27 -0400

    feat: added reserve router to BaseRoutes

--- src/utils/BaseRoutes.ts
+++ src/utils/BaseRoutes.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import UserRouter from '../modules/user/UserRoutes'
 import carRouter from '../modules/car/CarRoutes'
+import reserveRouter from '../modules/reserve/ReserveRoutes'
 import ErrorController from '../errors/ErrorController';
 
 const app = express.Router();
@@ -8,6 +9,7 @@ const app = express.Router();
 app.use(express.json());
 app.use("/api/v1/user", UserRouter);
 app.use("/api/v1/car", carRouter);
+app.use("/api/v1/reserve", reserveRouter)
 app.use(ErrorController.globalErrorHandler)
 
 export default app;
